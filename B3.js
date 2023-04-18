0bd840181b13fd21d5544daae15bc6313b63a621
Your Name <tranhoangloc502@gmail.com>
2023-04-18 13:59:52 +0700

    done

--- B3.js
+++ B3.js
@@ -1,28 +1,16 @@
-function combination(n, k) {
-    if (k === 0 || k === n) {
-        return 1;
-    } else {
-        return combination(n - 1, k - 1) + combination(n - 1, k);
-    }
-}
+const A = [1, 2, 3];
 
-function countPairs(A, n) {
-    let pairs = [];
-    let numPairs = combination(n, 2);
-    let k = 0;
-    for (let i = 0; i < n - 1; i++) {
-        for (let j = i + 1; j < n; j++) {
-            pairs.push([A[i], A[j]]);
-            k++;
+const n = 2;
+
+
+for (let i = 0; i < A.length; i++) {
+    for (let j = i + 1; j < A.length; j++) {
+        if (n === 2) {
+            console.log(A[i] + '&' + A[j]);
+        } else {
+            for (let k = j + 1; k < A.length; k++) {
+                console.log(A[i] + '&' + A[j] + '&' + A[k]);
+            }
         }
     }
-    console.log("Kết quả có", numPairs, "cặp số:");
-    for (let i = 0; i < pairs.length; i++) {
-        console.log(pairs[i][0] + "&" + pairs[i][1]);
-    }
 }
-
-
-let A = [1, 2, 3 , 4];
-let n = A.length;
-countPairs(A, n);
